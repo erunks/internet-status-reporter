fb7a18d2c3db1a11333f9d4ad8ac38ef1d712b68
Edward Runkel <eddierunkel@gmail.com>
2020-09-10 15:53:21 -0400

    Rough working class version of the modem reporter. Does not interact with the DB yet

--- src/ModemReporter.py
+++ src/ModemReporter.py
@@ -1,48 +1,111 @@
-from dotenv import load_dotenv, find_dotenv
-from mechanicalsoup import StatefulBrowser
 from os import getenv
-from pprint import pprint
 
-def format_data(data):
-  return data.string.encode('ascii', 'ignore').decode('utf-8').strip()
+class ModemReporter:
+  def __init__(self):
+    from dotenv import load_dotenv, find_dotenv
+    load_dotenv(find_dotenv())
 
-def scrape_page():
-  EVENT_PAGE = getenv('MODEM_ADDRESS') + '/MotoSnmpLog.asp'
-  browser.open(EVENT_PAGE)
+    MODEM_ADDRESS = getenv('MODEM_ADDRESS')
 
-  table_content = browser.get_current_page().find('table', class_='moto-table-content')
-  for table_row in table_content.find_all('tr'):
-    if not 'bgcolor' in table_row.attrs:
-      continue
-    else:
-      data = list(table_row.children)
+    self.__setup_browser()
+    self.db = None
+    self.pages = {
+      'home': MODEM_ADDRESS,
+      'event_log': MODEM_ADDRESS + '/MotoSnmpLog.asp'
+    }
 
-      time = format_data(data[0])
-      priority = format_data(data[1])
-      description = format_data(data[2])
+  def __del__(self):
+    if self.db != None:
+      self.db.close()
 
-      data = [time, priority, description]
-      if "".join(data) == "":
+  def __setup_browser(self):
+    from mechanicalsoup import StatefulBrowser
+
+    self.browser = StatefulBrowser()
+    self.browser.set_verbose(2)
+
+  def run(self):
+    self.login()
+    self.scrape_events()
+
+  def login(self):
+    self.browser.open(self.pages['home'])
+
+    submit_button = self.browser.get_current_page().find('input', class_='moto-login-button')
+
+    form = self.browser.select_form()
+    form.set('loginUsername', getenv('MODEM_USERNAME'))
+    form.set('loginPassword', getenv('MODEM_PASSWORD'))
+    form.choose_submit(submit_button)
+
+    self.browser.submit_selected()
+
+  def report_events(self, events):
+    from mysql.connector import Error as DB_Error, ProgrammingError, connect, errorcode
+    from utils import format_modem_priority_as_int, format_modem_time_as_datetime
+
+    try:
+      self.db = connect(
+        database = getenv('DATABASE'),
+        host = getenv('DB_HOST'),
+        password = getenv('DB_PASSWORD'),
+        user = getenv('DB_USERNAME')
+      )
+
+      cursor = self.db.cursor()
+      sql = 'INSERT INTO `modem_events` (`description`, `priority`, `created_at`, `maintenance`) VALUES (%s, %s, %s, %s)'
+
+      for event in events:
+        time, priority, description = event
+        created_at = str(format_modem_time_as_datetime(time))
+
+        values = (description, format_modem_priority_as_int(priority), created_at, False)
+        cursor.execute(sql, values)
+
+      self.db.commit()
+      cursor.close()
+    except ProgrammingError as err:
+      if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        self.logger.error(f'Access denied: {err}')
+      else:
+        self.logger.exception(f'Unexpected error: {err}')
+    except DB_Error as err:
+      if err.errno == errorcode.ER_BAD_DB_ERROR:
+        self.logger.error(f'Database does not exist: {err}')
+      else:
+        self.logger.exception(f'Unexpected error: {err}')
+    finally:
+      if self.db != None:
+        self.db.close()
+        self.db = None
+
+  def scrape_events(self):
+    from pprint import pprint
+
+    self.browser.open(self.pages['event_log'])
+
+    table_content = self.browser.get_current_page().find('table', class_='moto-table-content')
+
+    event_logs = []
+    for table_row in table_content.find_all('tr'):
+      if not 'bgcolor' in table_row.attrs:
         continue
+      else:
+        data = list(table_row.children)
 
-      pprint(data)
+        time = data[0].string.strip()
+        priority = data[1].string.strip()
+        description = data[2].string.strip()
 
-def login():
-  LOGIN_PAGE = getenv('MODEM_ADDRESS')
-  browser.open(LOGIN_PAGE)
+        data = [time, priority, description]
+        if "".join(data) == "":
+          continue
 
-  form_submit = browser.get_current_page().find('input', class_='moto-login-button')
+        event_logs.append(data)
+        pprint(data)
+    
+    return event_logs
 
-  form = browser.select_form()
-  form.set('loginUsername', getenv('MODEM_USERNAME'))
-  form.set('loginPassword', getenv('MODEM_PASSWORD'))
-  form.choose_submit(form_submit)
-  response = browser.submit_selected()
-
-
-load_dotenv(find_dotenv())
-
-browser = StatefulBrowser()
-browser.set_verbose(2)
-login()
-scrape_page()
+  
+mr = ModemReporter()
+mr.run()
