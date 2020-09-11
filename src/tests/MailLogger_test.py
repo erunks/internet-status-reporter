07f9bb2d730b3436b7c4256d6f5864fc01212efd
Edward Runkel <eddierunkel@gmail.com>
2020-09-10 23:00:35 -0400

    Address issues raised by codacy

--- src/tests/MailLogger_test.py
+++ src/tests/MailLogger_test.py
@@ -3,19 +3,21 @@ from unittest.mock import MagicMock, patch
 from src.MailLogger import MailLogger
 
 class TestMailLogger(unittest.TestCase):
+  @classmethod
+  def setUpClass(self):
+    self.logger = MagicMock()
+    self.mail_handler = MagicMock()
+
   @patch('logging.getLogger')
   @patch('src.MailHandler.MailHandler')
   def test_mail_logger_initializes_correctly(self, mail_handler_mock, get_logger_mock):
-    mail_handler_instance_mock = MagicMock()
-    mail_handler_mock.return_value = mail_handler_instance_mock
-
-    logger_mock = MagicMock()
-    get_logger_mock.return_value = logger_mock
+    mail_handler_mock.return_value = self.mail_handler
+    get_logger_mock.return_value = self.logger
 
     MailLogger()
 
-    mail_handler_instance_mock.setLevel.assert_called_once()
-    mail_handler_instance_mock.setFormatter.assert_called_once()
+    self.mail_handler.setLevel.assert_called_once()
+    self.mail_handler.setFormatter.assert_called_once()
     get_logger_mock.assert_called_once_with('MailLogger_Logger')
-    logger_mock.addHandler.assert_called_once_with(mail_handler_instance_mock)
-    logger_mock.info.assert_called_once_with('MailLogger is starting up')
+    self.logger.addHandler.assert_called_once_with(self.mail_handler)
+    self.logger.info.assert_called_once_with('MailLogger is starting up')
